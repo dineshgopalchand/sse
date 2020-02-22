5c43601e65156d734f39bacf40ae89db37dbb1fb
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-02-22 11:19:05 +0530

    Added bootstrap module and box-container, button-box container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonBoxComponent } from './button-box/button-box.component';
+import { BoxContainerComponent } from './box-container/box-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonBoxComponent,
+    BoxContainerComponent
   ],
   imports: [
     BrowserModule,
